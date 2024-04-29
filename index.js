12e0889d06f1af295341b3320548586c865fb833
evansightcall <113928516+evansightcall@users.noreply.github.com>
2024-04-29 09:07:18 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 var uainfo = navigator.userAgentData;
-console.log(uainfo);
+//console.log(uainfo);
+document.getElementById("uaDataP").html(
+  JSON.stringify(
+    uainfo.getHighEntropyValues(['fullVersionList','architecture','bitness','formFactor','model','wow64']).then((res)=>{console.log(res); return res;}),
+    2
+  )
+);
