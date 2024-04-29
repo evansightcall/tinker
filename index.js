6c62313ad938b8d51974d8c84596efdc57032e31
evansightcall <113928516+evansightcall@users.noreply.github.com>
2024-04-29 09:25:37 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,27 +1,3 @@
-function syntaxHighlight(json) {
-    if (typeof json != 'string') {
-        json = JSON.stringify(json, undefined, 2);
-    }
-    json = json.replace(/&/g, '&').replace(/</g, '<').replace(/>/g, '>');
-    return json.replace(/(\"(\\\\u[a-zA-Z0-9]{4}|\\\\[^u]|[^\\\\\"])*\"(\\s*:)?|\\b(true|false|null)\\b|-?\\d+(?:\\.\\d*)?(?:[eE][+\\-]?\\d+)?)/g, function (match) {
-        var cls = 'number';
-        if (/^\"/.test(match)) {
-            if (/:$/.test(match)) {
-                cls = 'key';
-            } else {
-                cls = 'string';
-            }
-        } else if (/true|false/.test(match)) {
-            cls = 'boolean';
-        } else if (/null/.test(match)) {
-            cls = 'null';
-        }
-        return '<span class="' + cls + '">' + match + '</span>';
-    });
-}
-
-
-
 var uainfo = navigator.userAgentData;
 //console.log(uainfo);
 
@@ -29,10 +5,7 @@ var uavals = uainfo.getHighEntropyValues(['fullVersionList','architecture','bitn
   .then(
     (res)=>{
       console.log(res);
-      //document.getElementById("uaDataP").innerHTML = JSON.stringify(res,null,2);
-      document.getElementById("uaDataP").innerHTML = syntaxHighlight(res);
+      document.getElementById("uaDataP").innerHTML = JSON.stringify(res,null,2);
       return res;
     }
   );
-
-
