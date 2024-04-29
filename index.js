21e82804457b19de25b039c6426cb84d30de2696
evansightcall <113928516+evansightcall@users.noreply.github.com>
2024-04-29 09:12:57 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 var uainfo = navigator.userAgentData;
 //console.log(uainfo);
-document.getElementById("uaDataP").innerHTML(
-  JSON.stringify(
-    uainfo.getHighEntropyValues(['fullVersionList','architecture','bitness','formFactor','model','wow64']).then((res)=>{console.log(res); return res;}),
-    2
-  )
+document.getElementById("uaDataP").innerHTML =
+JSON.stringify(
+  uainfo.getHighEntropyValues(['fullVersionList','architecture','bitness','formFactor','model','wow64']).then((res)=>{console.log(res); return res;}),
+  2
 );
